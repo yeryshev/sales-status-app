9e1fc2da66ab33959837bfd20eec7610b0f2ae52
Vadim Eryshev <yeryshev@gmail.com>
2024-03-01 18:09:00 +0300

    add prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,23 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    indent: [2, 4],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@mui/*/*/*'],
+      },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
-    rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        indent: [2, 4],
-        'no-restricted-imports': [
-            'error',
-            {
-                patterns: ['@mui/*/*/*'],
-            },
-        ],
-    },
+  },
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './app/redux/store.ts'
-import { SocketCtxProvider } from './app/providers/WsProvider/index.ts'
-import { ColorModeCtxProvider } from './app/providers/ThemeProvider/index.ts'
-import App from './app/App.tsx'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/redux/store.ts';
+import { SocketCtxProvider } from './app/providers/WsProvider/index.ts';
+import { ColorModeCtxProvider } from './app/providers/ThemeProvider/index.ts';
+import App from './app/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
@@ -16,4 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             </SocketCtxProvider>
         </ColorModeCtxProvider>
     </Provider>
-)
+);
