2a3a80160a174836f1a158aa8c0f82a0950ecbdc
Vadim Eryshev <yeryshev@gmail.com>
2024-03-01 16:32:01 +0300

    add vitest

---
+++ config/vitest/vitest.config.ts
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        include: ['**/*.test.tsx', '**/*.test.ts'],
+        globals: true,
+    },
+});
