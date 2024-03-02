4e51c5488711fa33d966aab66f7622217432c901
Vadim Eryshev <yeryshev@gmail.com>
2024-03-02 20:23:10 +0300

    add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import { store } from './app/redux/store.ts';
 import { SocketCtxProvider } from './app/providers/WsProvider/index.ts';
 import { ColorModeCtxProvider } from './app/providers/ThemeProvider/index.ts';
 import App from './app/App.tsx';
+import { ErrorBoundary } from './app/providers/ErrorBoundary/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <ColorModeCtxProvider>
-            <SocketCtxProvider>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </SocketCtxProvider>
+            <ErrorBoundary>
+                <SocketCtxProvider>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </SocketCtxProvider>
+            </ErrorBoundary>
         </ColorModeCtxProvider>
     </Provider>
 );
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,16 @@
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './PageError.module.scss';
+import { Button, Typography } from '@mui/material';
+
+export const PageError = () => {
+    const reloadPage = () => {
+        window.location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.PageError, {}, [])}>
+            <Typography variant="h6">Произошла непредвиденная ошибка</Typography>
+            <Button onClick={reloadPage}>Обновить страницу</Button>
+        </div>
+    );
+};
