a5a4244e83a6bfcfc2b58080db39df72acc6e6c0
Vadim Eryshev <yeryshev@gmail.com>
2024-03-28 18:08:08 +0300

    add tests for profile page selectors

---
+++ src/pages/ProfilePage/model/selectors/getProfileError/getProfileError.test.ts
@@ -0,0 +1,21 @@
+import { StateSchema } from '@/app/providers/StoreProvider';
+import { getProfileError } from './getProfileError';
+import { describe, expect, test } from 'vitest';
+
+describe('getProfileError', () => {
+    test('should return profile error', () => {
+
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                error: 'error',
+            },
+        };
+
+        expect(getProfileError(state as StateSchema)).toEqual('error');
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileError(state as StateSchema)).toEqual(undefined);
+    });
+});
---
+++ src/pages/ProfilePage/model/selectors/getProfileForm/getProfileForm.test.ts
@@ -0,0 +1,30 @@
+import { StateSchema } from '@/app/providers/StoreProvider';
+import { getProfileForm } from './getProfileForm';
+import { describe, expect, test } from 'vitest';
+
+describe('getProfileForm', () => {
+    test('should return profile form', () => {
+        const data = {
+            firstName: 'firstName',
+            secondName: 'secondName',
+            email: 'example@example.com',
+            photoUrl: 'photoUrl',
+            extNumber: 'extNumber',
+            insideId: 1,
+            telegram: 'telegram',
+            isWorkingRemotely: true,
+        };
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                form: data,
+            },
+        };
+
+        expect(getProfileForm(state as StateSchema)).toEqual(data);
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileForm(state as StateSchema)).toEqual(undefined);
+    });
+});
