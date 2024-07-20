457ec5ce0fd016ecbcecd98fe9cd64d9a8fe0a0c
Vadim Eryshev <yeryshev@gmail.com>
2024-07-20 19:37:20 +0300

    Removed StatusBox and status entity selectors

--- frontend/src/entities/Status/index.ts
+++ frontend/src/entities/Status/index.ts
@@ -1,4 +1,3 @@
-export { StatusBox } from './ui/StatusBox';
 export { statusReducer, statusActions } from './model/slice/statusSlice';
 export { type StatusSchema } from './model/types/StatusSchema';
 export { type Status } from './model/types/Status';
--- frontend/src/entities/Status/model/selectors/getStatusValue/getStatusData.ts
+++
@@ -1,4 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { getStatus } from '../getStatus/getStatus';
-
-export const getStatusData = createSelector(getStatus, (status) => status.data);
