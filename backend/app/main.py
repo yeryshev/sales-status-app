d3798d28d758126b48a7d163b54ae9f63a9ad41f
Vadim Eryshev <yeryshev@gmail.com>
2024-07-28 18:58:37 +0300

    Added sentry to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
+import sentry_sdk
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
 
-app = FastAPI(title='Team Status API')
+if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
+    sentry_sdk.init(
+        dsn=str(settings.SENTRY_DSN),
+        traces_sample_rate=0.1,
+        profiles_sample_rate=0.1,
+        enable_tracing=True,
+    )
+
+app = FastAPI(title="Team Status API")
 
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
@@ -14,7 +23,15 @@ if settings.BACKEND_CORS_ORIGINS:
         ],
         allow_credentials=True,
         allow_methods=["POST", "GET", "OPTIONS", "PUT", "DELETE", "PATCH"],
-        allow_headers=["Access-Control-Allow-Origin", "Content-Type", "Authorization", "Accept", "Origin",
-                       "sentry-trace", "baggage"])
+        allow_headers=[
+            "Access-Control-Allow-Origin",
+            "Content-Type",
+            "Authorization",
+            "Accept",
+            "Origin",
+            "sentry-trace",
+            "baggage",
+        ],
+    )
 
 app.include_router(api_router)
