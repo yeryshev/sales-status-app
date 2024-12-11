912bd0d54cf6aeb063ce016636cca82d3bc14181
Vadim Eryshev <yeryshev@gmail.com>
2024-12-11 18:42:14 +0300

    Update eslint-plugin-fsd version

--- frontend/src/widgets/TablesBox/ui/TeamTable/TeamTable.tsx
+++ frontend/src/widgets/TablesBox/ui/TeamTable/TeamTable.tsx
@@ -15,7 +15,7 @@ import {
 } from '@/entities/Team';
 import { RowSkeleton } from '../RowSkeleton/RowSkeleton';
 import { getTeamTableHeadersList } from './Headers/getTeamTableHeadersList';
-import { TeamTableHeaderItem } from '../TeamTable/Headers/TeamTableHeaderItem';
+import { TeamTableHeaderItem } from './Headers/TeamTableHeaderItem';
 
 interface TeamTableProps {
   teamList: Teammate[];
