90a90d7e5f1b0d361059de10f914cae56d2eab98
Vadim Eryshev <yeryshev@gmail.com>
2024-12-15 00:33:59 +0300

    Remove Celery and Redis dependencies.
    Eliminated Celery and Redis configurations, code, and dependencies to streamline the backend. Updated the Docker Compose file, removed related packages from `poetry.lock` and `pyproject.toml`, and cleaned the task-related code base.

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -1,6 +1,3 @@
-import os
-
-from celery import Celery
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
@@ -11,12 +8,6 @@ from app.utils import (
     change_mango_status,
     mango_statuses,
     send_ws_with_all_users,
-)
-
-celery = Celery("tasks")
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://localhost:6379"
 )
 
 offline_status_id = app_statuses["offline"][0]
@@ -43,27 +34,3 @@ async def set_offline_users():
         await send_ws_with_all_users(updated_users)
     finally:
         await session.close()
-
-
-# @celery.on_after_configure.connect
-# def setup_periodic_tasks(sender, **kwargs):  # noqa: ARG001
-#     sender.add_periodic_task(
-#         crontab(hour="16", minute="00"),
-#         set_offline_users.s(),
-#         name="set_offline_users_every_day_at_16_00_UTC",
-#     )
-#
-#
-# @celery.task(name="set_offline_users")
-# def set_offline_users():
-#     with sync_session_factory() as session:
-#         users_to_update = (
-#             session.query(User).filter(User.status_id != offline_status_id).all()
-#         )
-#
-#         for user in users_to_update:
-#             user.status_id = offline_status_id
-#             change_mango_status(user, mango_statuses["offline"])
-#         session.commit()
-#         session.close()
-#     return "All users are offline now"
